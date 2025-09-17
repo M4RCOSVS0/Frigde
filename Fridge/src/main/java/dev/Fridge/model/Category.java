88ac3d9da1e232078b1b2d60fa7efb2415187424
M4RCOSVS0 <marcossvin.so@gmail.com>
2025-09-17 17:02:31 -0300

    mudanças feitas

---
+++ Fridge/src/main/java/dev/Fridge/model/Category.java
@@ -0,0 +1,60 @@
+package dev.Fridge.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "category_table")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Food> foods = new HashSet<>();
+
+    // Construtores, getters e setters
+    public Category() {
+    }
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(Set<Food> foods) {
+        this.foods = foods;
+    }
+
+}
